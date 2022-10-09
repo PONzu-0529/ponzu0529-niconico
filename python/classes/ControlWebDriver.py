8ff39baae9de1615e47b40b0e92bf8b1b0a84e17
PONzu-0529 <yatponzu@gmail.com>
2022-10-09 15:20:03 +0900

    Add Control Web Driver Fixes #6 (Test is not completed)

---
+++ python/classes/ControlWebDriver.py
@@ -0,0 +1,87 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.remote.webdriver import WebElement
+
+
+class ControlWebDriver():
+
+    driver: WebDriver = None
+
+
+    def __init__(self, selenium_server_host: str, selenium_server_port = '4444'):
+        """
+        Constructor
+
+        * selenium_server_host:
+        * selenium_server_port: (default 4444)
+        """
+
+        options = Options()
+
+        options.add_argument("--headless")
+        options.add_argument('--mute-audio')
+        options.add_argument('--no-sandbox')
+        options.add_experimental_option('excludeSwitches', ['enable-logging'])
+
+        self.driver = webdriver.Remote(
+            command_executor = "http://%s:%s" % (selenium_server_host, selenium_server_port),
+            options = options
+        )
+
+
+    def __del__(self):
+        """
+        Destructor
+        """
+
+        self.delete_driver()
+
+
+    def access_the_url(self, url: str):
+        """
+        Loads a web page in the current browser session.
+        * url: target url
+        """
+
+        self.driver.get(url)
+
+
+    def find_element_by_id(self, id: str, elem: WebElement = None) -> WebElement:
+        """
+        Finds an element by id.
+        * id: ID Name
+        * elem: WebDriver's Element or WebElement(default None)
+        """
+
+        if (elem == None):
+
+            return self.driver.find_element_by_id(id)
+
+        else:
+
+            return elem.find_element_by_id(id)
+
+
+    def find_elements_by_tag_name(self, tag: str, elem: WebElement = None) -> list[WebElement]:
+        """
+        Finds a list of elements within this element's children by tag name.
+        * tag: Tag Name
+        * elem: WebDriver's Element or WebElement(default None)
+        """
+
+        if (elem == None):
+
+            return self.driver.find_elements_by_tag_name(tag)
+
+        else:
+
+            return elem.find_elements_by_tag_name(tag)
+
+
+    def delete_driver(self):
+        """
+        Quits the driver and closes every associated window.
+        """
+
+        self.driver.quit()
--- test/test_selenium.py
+++ test/test_selenium.py
@@ -1,11 +1,16 @@
 import os
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.remote.webdriver import WebElement
 
 
 def main():
 
+    SELENIUM_SERVER_HOST = 'selenium'
     SELENIUM_SERVER_PORT: int = os.environ.get('SELENIUM_SERVER_PORT', '4444')
+
+    GITHUB_USER_NAME = 'PONzu-0529'
+    GITHUB_REPOSITORY_NAME = 'ponzu0529-niconico'
 
     option = Options()
 
@@ -15,7 +20,7 @@ def main():
     option.add_experimental_option('excludeSwitches', ['enable-logging'])
 
     driver = webdriver.Remote(
-        command_executor="http://selenium:%s" % (SELENIUM_SERVER_PORT),
+        command_executor="http://%s:%s" % (SELENIUM_SERVER_HOST, SELENIUM_SERVER_PORT),
         options=option
     )
 
@@ -23,11 +28,13 @@ def main():
 
         driver.maximize_window()
 
-        driver.get('https://github.com/PONzu-0529/ponzu0529-niconico')
+        driver.get("https://github.com/%s/%s" % (GITHUB_USER_NAME, GITHUB_REPOSITORY_NAME))
 
-        driver.save_screenshot('test.png')
+        repositoryContainerHeaderElem = driver.find_element_by_id('repository-container-header')
 
-        input('Press Enter...')
+        aTags: list[WebElement] = repositoryContainerHeaderElem.find_elements_by_tag_name('a')
+
+        print(aTags[1].text)
 
     except Exception as e:
 
