f90eec004a39ed7ae06c44fcdf609f56456e935e
PONzu-0529 <yatponzu@gmail.com>
2022-10-15 11:12:37 +0900

    Change Path in Copy Shell

--- copy_files.sh
+++ copy_files.sh
@@ -3,5 +3,5 @@
 cp -r .devcontainer/ default/
 cp -r .vscode/ default/
 cp -r mariadb/ default/
-cp home/php/config/Config.php cron/config/Config.php
-cp cron/config/Config.php default/config/Config.php
+cp -r home/php/config/ cron/
+cp -r cron/config/ default/
