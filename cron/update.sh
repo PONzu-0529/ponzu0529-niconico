98b0d3dec7288716377712671c95fc5cddeb1ee6
PONzu-0529 <yatponzu@gmail.com>
2023-02-27 08:48:01 +0900

    Fix Update Script for Xserver

--- cron/update.sh
+++ cron/update.sh
@@ -1,9 +1,9 @@
 ROOT=$1
 
-COMMAND='git pull' && OUTPUT=`cd $ROOT && $COMMAND` && php $ROOT/cron/commandLog.php "$COMMAND" "$OUTPUT"
-COMMAND='composer install' && OUTPUT=`cd $ROOT && $COMMAND` && php $ROOT/cron/commandLog.php "$COMMAND" "$OUTPUT"
-COMMAND='yarn' && OUTPUT=`cd $ROOT && $COMMAND` && php $ROOT/cron/commandLog.php "$COMMAND" "$OUTPUT"
-COMMAND='php artisan key:generate' && OUTPUT=`cd $ROOT && $COMMAND` && php $ROOT/cron/commandLog.php "$COMMAND" "$OUTPUT"
-COMMAND='php artisan migrate' && OUTPUT=`cd $ROOT && $COMMAND` && php $ROOT/cron/commandLog.php "$COMMAND" "$OUTPUT"
-COMMAND='php artisan db:seed' && OUTPUT=`cd $ROOT && $COMMAND` && php $ROOT/cron/commandLog.php "$COMMAND" "$OUTPUT"
-COMMAND='yarn build-full' && OUTPUT=`cd $ROOT && $COMMAND` && php $ROOT/cron/commandLog.php "$COMMAND" "$OUTPUT"
+COMMAND='git pull' && OUTPUT=`cd $ROOT && $COMMAND` && /usr/bin/php7.4 $ROOT/cron/commandLog.php "$COMMAND" "$OUTPUT"
+COMMAND='composer install' && OUTPUT=`cd $ROOT && $COMMAND` && /usr/bin/php7.4 $ROOT/cron/commandLog.php "$COMMAND" "$OUTPUT"
+COMMAND='yarn' && OUTPUT=`cd $ROOT && $COMMAND` && /usr/bin/php7.4 $ROOT/cron/commandLog.php "$COMMAND" "$OUTPUT"
+COMMAND='/usr/bin/php7.4 artisan key:generate' && OUTPUT=`cd $ROOT && $COMMAND` && /usr/bin/php7.4 $ROOT/cron/commandLog.php "$COMMAND" "$OUTPUT"
+COMMAND='/usr/bin/php7.4 artisan migrate' && OUTPUT=`cd $ROOT && $COMMAND` && /usr/bin/php7.4 $ROOT/cron/commandLog.php "$COMMAND" "$OUTPUT"
+COMMAND='/usr/bin/php7.4 artisan db:seed' && OUTPUT=`cd $ROOT && $COMMAND` && /usr/bin/php7.4 $ROOT/cron/commandLog.php "$COMMAND" "$OUTPUT"
+COMMAND='yarn build-full' && OUTPUT=`cd $ROOT && $COMMAND` && /usr/bin/php7.4 $ROOT/cron/commandLog.php "$COMMAND" "$OUTPUT"
