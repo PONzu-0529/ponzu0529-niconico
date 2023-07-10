a025d03b6cd3f90ddeae71ebdee403489ff45021
PONzu-0529 <yatponzu@gmail.com>
2023-07-10 23:56:22 +0900

    Add AWS Instance Start and Stop #112

--- app/Services/AWSService.php
+++ app/Services/AWSService.php
@@ -7,27 +7,119 @@ use App\Helpers\SettingHelper;
 
 class AWSService
 {
+    private int $ATTEMPT_COUNT = 10;
+    private int $WAIT_TIME = 5;
+
     /**
      * Get AWS EC2 Instance Status
      *
      * @param string $instance_id Instance ID
      * @return void
      */
-    public function getInstanceStatus(string $instance_id): void
+    public function isInstanceRunning(string $instance_id): bool
+    {
+        $output = $this->executeCommand(
+            "aws ec2 describe-instance-status --instance-ids \"{$instance_id}\"",
+            true,
+            'EC2 Instance ID is Invalid.'
+        );
+
+        return count($output['InstanceStatuses']) > 0;
+    }
+
+    public function startInstance(string $instance_id): void
+    {
+        if ($this->isInstanceRunning($instance_id)) {
+            throw new Exception('This Instance is Already Started.');
+        }
+
+        $this->executeCommand("aws ec2 start-instances --instance-ids \"{$instance_id}\"", true);
+
+        $count = 0;
+        $isInstanceRunning = false;
+
+        while ($count < $this->ATTEMPT_COUNT) {
+            $isInstanceRunning = $this->isInstanceRunning($instance_id);
+
+            if ($isInstanceRunning) {
+                break;
+            }
+
+            sleep($this->WAIT_TIME);
+
+            $count++;
+        }
+
+        if (!$isInstanceRunning) {
+            throw new Exception('Failed to Start Instance.');
+        }
+    }
+
+    public function stopInstance(string $instance_id): void
+    {
+        if (!$this->isInstanceRunning($instance_id)) {
+            throw new Exception('This Instance is Already Stopped.');
+        }
+
+        $this->executeCommand("aws ec2 stop-instances --instance-ids \"{$instance_id}\"", true);
+
+        $count = 0;
+        $isInstanceRunning = true;
+
+        while ($count < $this->ATTEMPT_COUNT) {
+            $isInstanceRunning = $this->isInstanceRunning($instance_id);
+
+            if (!$isInstanceRunning) {
+                break;
+            }
+
+            sleep($this->WAIT_TIME);
+
+            $count++;
+        }
+
+        if ($isInstanceRunning) {
+            throw new Exception('Failed to Stop Instance.');
+        }
+    }
+
+    public function getInstanceIPAddress(string $instance_id): string
+    {
+        if (!$this->isInstanceRunning($instance_id)) {
+            throw new Exception('This Instance is not Started.');
+        }
+
+        $output = $this->executeCommand(
+            "aws ec2 describe-instances --instance-ids \"{$instance_id}\" --query 'Reservations[].Instances[].PublicIpAddress' --output text"
+        );
+
+        return $output;
+    }
+
+    /**
+     * Execute Command
+     *
+     * @param string $command Command
+     * @param string $exception_message Exception Message
+     * @return string | array Output
+     */
+    private function executeCommand(string $command, bool $is_output_array = false, string $exception_message = '')
     {
         $this->setSettings();
 
-        exec("aws ec2 describe-instance-status --instance-ids \"{$instance_id}\"", $output_list);
+        exec($command, $output);
 
-        if (count($output_list) === 0) {
-            throw new Exception('EC2 Instance ID is Invalid.');
+        if (!$is_output_array) {
+            return $output[0];
         }
 
-        $output_json = str_replace(' ', '', implode('', $output_list));
+        if (count($output) === 0) {
+            throw new Exception($exception_message ?? 'Command is Invalid.');
+        }
 
-        $output = json_decode($output_json, true);
+        $output_json = str_replace(' ', '', implode('', $output));
 
-        var_dump($output);
+        return json_decode($output_json, true);
     }
 
     /**
--- tests/Unit/Services/AWSServiceTest.php
+++ tests/Unit/Services/AWSServiceTest.php
@@ -13,9 +13,9 @@ class AWSServiceTest extends TestCase
     {
         $service = new AWSService();
 
-        $service->getInstanceStatus(SettingHelper::getSettingValue('SELENIUM_STANDALONE_INSTANCE_ID'));
+        $service->isInstanceRunning(SettingHelper::getSettingValue('SELENIUM_STANDALONE_INSTANCE_ID'));
 
-        $this->assertEquals('', '');
+        $this->assertTrue(true);
     }
 
     public function test_fail_aws_start_instance_no_instance_id()
@@ -23,7 +23,7 @@ class AWSServiceTest extends TestCase
         $service = new AWSService();
 
         try {
-            $service->getInstanceStatus('');
+            $service->isInstanceRunning('');
         } catch (Exception $ex) {
             $this->assertEquals($ex->getMessage(), 'EC2 Instance ID is Invalid.');
         }
@@ -34,9 +34,40 @@ class AWSServiceTest extends TestCase
         $service = new AWSService();
 
         try {
-            $service->getInstanceStatus('i-dummy');
+            $service->isInstanceRunning('i-dummy');
         } catch (Exception $ex) {
             $this->assertEquals($ex->getMessage(), 'EC2 Instance ID is Invalid.');
         }
+    }
+
+    public function test_success_aws_start_and_stop_instance()
+    {
+        $service = new AWSService();
+
+        try {
+            $service->getInstanceIPAddress(SettingHelper::getSettingValue('SELENIUM_STANDALONE_INSTANCE_ID'));
+        } catch (Exception $ex) {
+            $this->assertEquals($ex->getMessage(), 'This Instance is not Started.');
+        }
+
+        $service->startInstance(SettingHelper::getSettingValue('SELENIUM_STANDALONE_INSTANCE_ID'));
+
+        try {
+            $service->startInstance(SettingHelper::getSettingValue('SELENIUM_STANDALONE_INSTANCE_ID'));
+        } catch (Exception $ex) {
+            $this->assertEquals($ex->getMessage(), 'This Instance is Already Started.');
+        }
+
+        $service->getInstanceIPAddress(SettingHelper::getSettingValue('SELENIUM_STANDALONE_INSTANCE_ID'));
+
+        $service->stopInstance(SettingHelper::getSettingValue('SELENIUM_STANDALONE_INSTANCE_ID'));
+
+        try {
+            $service->stopInstance(SettingHelper::getSettingValue('SELENIUM_STANDALONE_INSTANCE_ID'));
+        } catch (Exception $ex) {
+            $this->assertEquals($ex->getMessage(), 'This Instance is Already Stopped.');
+        }
+
+        $this->assertTrue(true);
     }
 }
