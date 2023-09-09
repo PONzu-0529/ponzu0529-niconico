e25e720ff5d673a85b224dc5c3f95084c3e06d9c
PONzu-0529 <yatponzu@gmail.com>
2023-09-09 15:03:36 +0900

    Update NicoMylistAutoGen

---
+++ src/Objects/NicoMylistAutoGen/CreateCustomMylistRequestObject.ts
@@ -0,0 +1,13 @@
+export default class CreateCustomMylistRequestObject {
+  /** Email */
+  email: string;
+
+  /** Password */
+  password: string;
+
+  /** Mylist Title */
+  mylist_title: string;
+
+  /** Count */
+  count: number;
+}
