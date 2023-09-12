e75a8bf0d515607763016b05d4be424fd29e2d63
PONzu-0529 <yatponzu@gmail.com>
2023-09-12 22:06:37 +0900

    Add Custom Button #148

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -1,14 +1,40 @@
 <template>
   <div>
-    Test
+    <div>
+      <custom-button
+        :option="defaultButton"
+        @clickEvent="() => {}"
+      />
+    </div>
+    <div>
+      <custom-button
+        :option="disabledButton"
+        @clickEvent="() => {}"
+      />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component } from 'vue-property-decorator';
 import BaseView from '@/views/BaseView.vue';
+import CustomButton, { CustomButtonOption } from '@/components/CustomButton.vue';
 
-@Component({})
+@Component({
+  components: {
+    CustomButton
+  }
+})
 export default class Test extends BaseView {
+  private defaultButton: CustomButtonOption = {
+    label: 'DefaultButton',
+    click: () => { alert('Clicked!!!'); }
+  }
+
+  private disabledButton: CustomButtonOption = {
+    label: 'DisabledButton',
+    disabled: true,
+    click: () => { return; }
+  }
 }
 </script>
