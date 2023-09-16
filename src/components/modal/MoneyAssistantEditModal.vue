160d4233fed9d8021dcefd7b9abbae9277a4ccd6
PONzu-0529 <yatponzu@gmail.com>
2023-09-16 20:55:49 +0900

    Add EditModal for MoneyAssistant #107

---
+++ src/components/modal/MoneyAssistantEditModal.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="modal-background">
+    <div class="modal-base">
+      <div class="l-money-assistant-edit-modal-option">
+        <custom-button :option="closeModalButtonOption" />
+      </div>
+      <div class="l-money-assistant-edit-modal-main">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop } from 'vue-property-decorator';
+import BaseView from '@/views/BaseView.vue';
+import CustomButton, { CustomButtonOption } from '@/components/CustomButton.vue';
+
+@Component({
+  components: {
+    CustomButton
+  }
+})
+export default class MoneyAssistantEditModal extends BaseView {
+  @Prop()
+  private option: MoneyAssistantEditModalOption;
+
+  private closeModalButtonOption: CustomButtonOption = {
+    label: '閉じる',
+    click: this.clickCloseModalButton
+  }
+
+  private clickCloseModalButton(): void {
+    this.option.clickCloseModalButton();
+  }
+}
+
+export interface MoneyAssistantEditModalOption {
+  clickCloseModalButton: () => void;
+}
+</script>
--- src/views/MoneyAssistant.vue
+++ src/views/MoneyAssistant.vue
@@ -1,13 +1,53 @@
 <template>
   <div class="l-content">
+    <money-assistant-edit-modal
+      v-show="editModalOpen"
+      :option="editModalOption"
+    />
+    <div class="l-money-assistant-option">
+      <custom-button :option="addRecordButtonOption" />
+    </div>
+    <div class="l-money-assistant-main">
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component } from 'vue-property-decorator';
 import BaseView from '@/views/BaseView.vue';
+import CustomButton, { CustomButtonOption } from '@/components/CustomButton.vue';
+import MoneyAssistantEditModal, { MoneyAssistantEditModalOption } from '@/components/modal/MoneyAssistantEditModal.vue';
 
-@Component({})
+@Component({
+  components: {
+    CustomButton,
+    MoneyAssistantEditModal
+  }
+})
 export default class MoneyAssistant extends BaseView {
+  private editModalOpen: boolean;
+
+  private editModalOption: MoneyAssistantEditModalOption = {
+    clickCloseModalButton: this.clickCloseModalButton
+  }
+
+  private addRecordButtonOption: CustomButtonOption = {
+    label: '追加',
+    click: this.clickAddRecordButton
+  }
+
+  constructor() {
+    super();
+
+    this.editModalOpen = false;
+  }
+
+  private clickCloseModalButton(): void {
+    this.editModalOpen = false;
+  }
+
+  private async clickAddRecordButton(): Promise<void> {
+    this.editModalOpen = true;
+  }
 }
 </script>
